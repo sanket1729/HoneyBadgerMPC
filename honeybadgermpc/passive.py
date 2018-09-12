a7629e42c8b48f393615fdbccb2f2266c6a9f1d3
hanyunxu <hanyunx2@illinois.edu>
2018-09-11 21:27:31 -0500

    add test_batchjubjub

--- honeybadgermpc/passive.py
+++ honeybadgermpc/passive.py
@@ -452,6 +452,71 @@ async def test_batchbeaver(context):
 
 # Read zeros from file, open them
 
+async def test_batchjubjub(context):
+
+    def mul(x, y):
+        a, b, ab = context.get_triple()
+        return beaver_mult(context, x, y, a, b, ab)
+
+    # Stream of random numbers for taking inverses
+    async def inverse(x):
+        _r = context.get_rand()
+        # return [r] / open([r * x])
+        rx = await (await mul(_r, x)).open()
+        return (1/rx) * _r
+
+    # ShareArray batch addition of points on jubjub
+
+    p1 = Point(0,1)
+    p2 = Point(5, 6846412461894745224441235558443359243034138132682534265960483512729196124138)
+    p3 = Point(10, 9069365299349881324022309154395348339753339814197599672892180073931980134853)
+    p4 = Point(20, 19591689915777126424527574975649725331665833659101192930707046924393354292087)
+    p5 = Point(21, 7077199607858622853608570958787827897736274549921838431359778037825087697958)
+    p6 = Point(28, 6872713299796450981547816838430986612693130632041108310621050902354381807248)
+
+    s1 = [p1, p2, p3]
+    s2 = [p4, p5, p6]
+    s3 = [] # for assertion with opened value
+
+    for p, q in zip(s1, s2):
+        s3.append(p + q)
+    assert len(s1) == len(s3)
+
+    x1s = [context.get_zero() + context.Share(i.x) for i in s1]
+    y1s = [context.get_zero() + context.Share(i.y) for i in s1]
+    x2s = [context.get_zero() + context.Share(i.x) for i in s2]
+    y2s = [context.get_zero() + context.Share(i.y) for i in s2]
+
+    x1s = context.ShareArray(x1s)
+    y1s = context.ShareArray(y1s)
+    x2s = context.ShareArray(x2s)
+    y2s = context.ShareArray(y2s)
+    x3s = []
+    y3s = []
+
+    for i, (x1, y1, x2, y2) in enumerate(
+            zip(x1s._shares, y1s._shares, x2s._shares, y2s._shares)):
+    
+        dx1x2y1y2 = p.curve.d * await mul(await mul(x1, x2), await mul(y1, y2))
+        x3num = (await mul(x1, y2) + await mul(y1, x2))
+        x3den = (context.Share(1) + dx1x2y1y2)
+        x3 = await mul(x3num, await inverse(x3den))
+        y3num = (await mul(y1, y2) + await mul(x1, x2))
+        y3den = (context.Share(1) - dx1x2y1y2)
+        y3 = await mul(y3num, await inverse(y3den))
+
+        x3s.append(x3)
+        y3s.append(y3)
+
+    x3s = context.ShareArray(x3s)
+    y3s = context.ShareArray(y3s)
+
+    X3s, Y3s = await x3s.open(), await y3s.open()
+
+    # verify
+    for i in range(len(s3)):
+        assert s3[i].x == X3s[i] and s3[i].y == Y3s[i]
+
 
 async def test_prog2(context):
 
@@ -491,15 +556,10 @@ async def test_prog3(context):
         rx = await (await mul(_r, x)).open()
         return (1/rx) * _r
 
-    # Example of jubjub
-    d = -(Field(10240)/Field(10241))
-    curve = Jubjub(Field(-1), d)
 
-    P = Point(curve,
-              Field(0x18ea85ca00cb9d895cb7b8669baa263fd270848f90ebefabe95b38300e80bde1),
+    P = Point(Field(0x18ea85ca00cb9d895cb7b8669baa263fd270848f90ebefabe95b38300e80bde1),
               Field(0x255fa75b6ef4d4e1349876df94ca8c9c3ec97778f89c0c3b2e4ccf25fdf9f7c1))
-    Q = Point(curve,
-              Field(0x1624451837683b2c4d2694173df71c9174ffcc613788eef3a9c7a7d0011476fa),
+    Q = Point(Field(0x1624451837683b2c4d2694173df71c9174ffcc613788eef3a9c7a7d0011476fa),
               Field(0x6f76dbfd7c62860d59f5937fa66d0571158ff68f28ccd83a4cd41b9918ee8fe2))
 
     R = P + Q
@@ -509,9 +569,8 @@ async def test_prog3(context):
     x2 = context.get_zero() + context.Share(Q.x)
     y2 = context.get_zero() + context.Share(Q.y)
 
-    # p = Point(curve, p_x, p_y)
-    # q = Point(curve, q_x, q_y)
-    dx1x2y1y2 = d * await mul(await mul(x1, x2), await mul(y1, y2))
+    
+    dx1x2y1y2 = P.curve.d * await mul(await mul(x1, x2), await mul(y1, y2))
     x3num = (await mul(x1, y2) + await mul(y1, x2))
     x3den = (context.Share(1) + dx1x2y1y2)
     x3 = await mul(x3num, await inverse(x3den))
@@ -523,9 +582,6 @@ async def test_prog3(context):
 
     assert X3 == R.x and Y3 == R.y
 
-    # print("P:", P)
-    # print("Q:", Q)
-    # print("P+Q:", P + Q)
 
 # Run some test cases
 if __name__ == '__main__':
@@ -540,15 +596,10 @@ if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.set_debug(True)
     try:
-<<<<<<< HEAD
         loop.run_until_complete(runProgramAsTasks(test_prog1, 3, 2))
         loop.run_until_complete(runProgramAsTasks(test_prog2, 3, 2))
         loop.run_until_complete(runProgramAsTasks(test_prog3, 3, 2))
-=======
-        loop.run_until_complete(runProgramInNetwork(test_prog1, 3, 2))
-        loop.run_until_complete(runProgramInNetwork(test_prog2, 3, 2))
-        loop.run_until_complete(runProgramInNetwork(test_prog3, 3, 2))
-        loop.run_until_complete(runProgramInNetwork(test_batchbeaver, 3, 2))
->>>>>>> ac57a9a... Added a ShareArray class
+        loop.run_until_complete(runProgramAsTasks(test_batchbeaver, 3, 2))
+        loop.run_until_complete(runProgramAsTasks(test_batchjubjub, 3, 2))
     finally:
         loop.close()
